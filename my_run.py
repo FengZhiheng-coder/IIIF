c6e938bdc4b7fab312e37939ba28d15182a90384
Zhiheng Feng <2795596712@qq.com>
2021-05-28 21:03:12 +0800

    这是一次上传测试，基本上没有修改

--- my_run.py
+++ my_run.py
@@ -12,6 +12,7 @@ D_C=np.array([[-0.018465666357146516],
             [0.0004638369263624063],
             [-0.0010282773327839974]])
 
+#to undistort image
 def undistort(img, K=K_C, D=D_C, DIM=DIM_C, scale=0.6, imshow=False):
     dim1 = img.shape[:2][::-1]  # dim1 is the dimension of input image to un-distort
     assert dim1[0] / dim1[1] == DIM[0] / DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
@@ -36,9 +37,9 @@ while (1):
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
-
     cv2.namedWindow("capture",cv2.WINDOW_NORMAL)
     cv2.imshow("capture", frame)
+
 
 cv2.destroyAllWindows()
 cap.release()
